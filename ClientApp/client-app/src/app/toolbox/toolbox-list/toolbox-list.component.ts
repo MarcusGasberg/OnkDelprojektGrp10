5e5d55414877efc870c103a78933b47556c6ad73
Edward Brunton <edward.brunton@me.com>
2020-03-02 15:36:43 +0100

    Added Tool View And Name

--- ClientApp/client-app/src/app/toolbox/toolbox-list/toolbox-list.component.ts
+++ ClientApp/client-app/src/app/toolbox/toolbox-list/toolbox-list.component.ts
@@ -7,14 +7,14 @@ import { catchError } from 'rxjs/operators';
 @Component({
   selector: 'app-toolbox-list',
   templateUrl: './toolbox-list.component.html',
-  styleUrls: ['./toolbox-list.component.scss']
+  styleUrls: ['./toolbox-list.component.scss'],
 })
 export class ToolboxListComponent implements OnInit {
   public toolboxes$: Observable<Toolbox[]>;
 
-  constructor(private service: ToolboxService, snackBar: ) { }
+  constructor(private service: ToolboxService) {}
 
   ngOnInit(): void {
-    this.toolboxes$ = this.service.fetchAll().pipe(catchError(err => ));
+    this.toolboxes$ = this.service.fetchAll().pipe();
   }
 }
